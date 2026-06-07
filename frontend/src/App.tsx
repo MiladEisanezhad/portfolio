e4b7329e32e8843f1641283962beabbc84571320
MiladEisanezhad <miladeisanezhad1@gmail.com>
2026-06-07 18:42:23 +0330

    updated skills

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -634,7 +634,7 @@ export default function App() {
                                 { name: 'SQLite', svg: <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg" alt="SQlite" className="w-10 h-10" /> },
                                 { name: 'MongoDB', svg: <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="MongoDB" className="w-10 h-10" /> },
                                 { name: 'REST API', svg: <svg viewBox="0 0 24 24" fill="currentColor"><path d="M14 6l-1-2H5v17h2v-7h5l1 2h7V6h-6zm4 8h-4l-1-2H7V6h5l1 2h5v6z" /></svg> },
-                                { name: 'Git', svg: <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="Git" className="w-10 h-10" /> },
+                                { name: 'Git', svg: <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" alt="Git" className="w-10 h-10" /> },
                             ].map((skill, i) => (
                                 <motion.div
                                     key={i}
