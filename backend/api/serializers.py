4b1424d4734e60ca2f1c56296341a7a3e7129a0d
MiladEisanezhad <miladeisanezhad1@gmail.com>
2026-06-12 14:20:56 +0330

    updated backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,17 +2,10 @@ from rest_framework import serializers
 from .models import Project, ContactMessage
 
 class ProjectSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
-
     class Meta:
         model = Project
         fields = '__all__'
 
-    def get_image(self, obj):
-        request = self.context.get('request')
-        if obj.image and request:
-            return request.build_absolute_uri(obj.image.url)
-        return None
 class ContactMessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactMessage
