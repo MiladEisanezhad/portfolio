e42952160ba684a906e37fd446c156bf7d0f7aa4
MiladEisanezhad <miladeisanezhad1@gmail.com>
2026-06-12 15:27:58 +0330

    updated backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Project, ContactMessage
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image_url = serializers.ImageField(use_url=True)
     class Meta:
         model = Project
         fields = '__all__'
